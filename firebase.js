db2b0fde1254d61885457cf4a28cf2b78bb73755
Adama Coulibaly <101523227+manimaneya@users.noreply.github.com>
2026-08-15 20:25:53 +0000

    Update Firebase configuration with new credentials

--- firebase.js
+++ firebase.js
@@ -21,13 +21,15 @@ import {
   serverTimestamp 
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "TON_API_KEY",
-  authDomain: "fissa-prod.firebaseapp.com",
-  projectId: "fissa-prod",
-  storageBucket: "fissa-prod.appspot.com",
-  messagingSenderId: "TON_SENDER_ID",
-  appId: "TON_APP_ID"
+  apiKey: "AIzaSyBM65_c3NRrXCRByN8wHIZ60OKLpqSG6m0",
+  authDomain: "fissa-1c083.firebaseapp.com",
+  projectId: "fissa-1c083",
+  storageBucket: "fissa-1c083.firebasestorage.app",
+  messagingSenderId: "403816254031",
+  appId: "1:403816254031:web:c601aebf6a3ef56982f83a",
+  measurementId: "G-V4HZBSWEG1"
 };
 
 const app = initializeApp(firebaseConfig);
